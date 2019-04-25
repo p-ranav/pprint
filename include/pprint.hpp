ab7b10b5bbc628503a74706a06db4e9eb2c5a808
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-24 21:59:23 -0400

    Pretty printer for std::vector

--- include/pprint.hpp
+++ include/pprint.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <typeinfo>
 #include <type_traits>
+#include <vector>
 #ifdef __GNUG__
 #include <cstdlib>
 #include <memory>
@@ -21,54 +22,46 @@ namespace pprint {
   class PrettyPrinter {
   public:
 
-    PrettyPrinter() :
-      indent_(0),
-      newline_(true) {}
-
     template <typename T>
     void print(T value) {
-      print_internal(value);
+      print_internal(value, 0, true, 0);
     }
 
   private:
 
     template <typename T>
     typename std::enable_if<std::is_integral<T>::value == true, void>::type
-    print_internal(T value) {
-      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    print_internal(T value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') << value << (newline ? "\n" : "");
     }
 
     template <typename T>
     typename std::enable_if<std::is_null_pointer<T>::value == true, void>::type
-    print_internal(T value) {
-      std::cout << std::string(indent_, ' ') << "null" << (newline_ ? "\n" : "");
+    print_internal(T value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') << "null" << (newline ? "\n" : "");
     }
 
-    void print_internal(float value) {
-      std::cout << std::string(indent_, ' ') << value << 'f' << (newline_ ? "\n" : "");
+    void print_internal(float value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') << value << 'f' << (newline ? "\n" : "");
     }
 
-    void print_internal(double value) {
-      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    void print_internal(double value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') << value << (newline ? "\n" : "");
     }
 
-    void print_internal(const std::string& value) {
-      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    void print_internal(const std::string& value, size_t indent = 0, bool newline = false,
+			size_t level = 0) {
+      std::cout << std::string(indent, ' ') << value << (newline ? "\n" : "");
     }
     
-    void print_internal(const char * value) {
-      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    void print_internal(const char * value, size_t indent = 0, bool newline = false,
+			size_t level = 0) {
+      std::cout << std::string(indent, ' ') << value << (newline ? "\n" : "");
     }    
 
-    void print_internal(bool value) {
-      std::cout << std::string(indent_, ' ') <<
-	(value ? "true" : "false") << (newline_ ? "\n" : "");
-    }
-
-    template <typename T>
-    typename std::enable_if<std::is_class<T>::value == true, void>::type
-    print_internal(T value) {
-      std::cout << std::string(indent_, ' ') << type(value) << (newline_ ? "\n" : "");
+    void print_internal(bool value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') <<
+	(value ? "true" : "false") << (newline ? "\n" : "");
     }
 
     std::string demangle(const char* name) {
@@ -87,10 +80,68 @@ namespace pprint {
     template <class T>
     std::string type(const T& t) {
       return demangle(typeid(t).name());
+    }    
+
+    template <typename T>
+    typename std::enable_if<std::is_class<T>::value == true, void>::type
+    print_internal(T value, size_t indent = 0, bool newline = false, size_t level = 0) {
+      std::cout << std::string(indent, ' ') << type(value) << (newline ? "\n" : "");
     }
-    
-    size_t indent_;
-    bool newline_;
+
+    template <typename T>
+    void print_internal(const std::vector<T>& value, size_t indent = 0, bool newline = false,
+			size_t level = 0) {
+      if (level == 0) {	
+	if (value.size() == 1) {
+	  print_internal("[", 0, false);
+	  print_internal(value.front(), 0, false, level + 1);
+	}
+	else if (value.size() > 0) {
+	  print_internal("[", 0, true);
+	  print_internal(value.front(), indent + 4, false, level + 1);
+	  if (value.size() > 1 && is_specialization<T, std::vector>::value == false)
+	    print_internal(", ", 0, true);
+	  else if (is_specialization<T, std::vector>::value)
+	    print_internal(", ", 0, true);
+	  for (size_t i = 1; i < value.size() - 1; i++) {
+	    print_internal(value[i], indent + 4, false, level + 1);
+	    if (is_specialization<T, std::vector>::value == false)
+	      print_internal(", ", 0, true);
+	    else
+	      print_internal(", ", 0, true);	    
+	  }
+	  if (value.size() > 1) {
+	    print_internal(value.back(), indent + 4, true, level + 1);
+	  }
+	}
+	if (is_specialization<T, std::vector>::value == false)
+	  print_internal("]\n");
+	else
+	  print_internal("\n]\n");
+      }
+      else {
+	if (value.size() == 1) {
+	  print_internal("[", indent, false);
+	  print_internal(value.front(), 0, false, level + 1);
+	}
+	else if (value.size() > 0) {
+	  print_internal("[", indent, false);
+	  print_internal(value.front(), 0, false, level + 1);
+	  if (value.size() > 1)
+	    print_internal(", ", 0, false);
+	  for (size_t i = 1; i < value.size() - 1; i++) {	      
+	    print_internal(value[i], 0, false, level + 1);
+	    print_internal(", ", 0, false);
+	  }
+	  if (value.size() > 1) {
+	    print_internal(value.back(), 0, false, level + 1);
+	  }
+	}
+	print_internal("]", 0, false);
+      }
+      
+    }
+
   };
   
 }
