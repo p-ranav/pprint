2655280683dd98ea248b2a531654e790b0453d33
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-24 20:54:26 -0400

    Added pprint for classes - Using gcc cxxabi to demangle

--- include/pprint.hpp
+++ include/pprint.hpp
@@ -1,5 +1,13 @@
+#pragma once
 #include <iostream>
+#include <string>
+#include <typeinfo>
 #include <type_traits>
+#ifdef __GNUG__
+#include <cstdlib>
+#include <memory>
+#include <cxxabi.h>
+#endif
 
 namespace pprint {
 
@@ -30,17 +38,55 @@ namespace pprint {
       std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
     }
 
+    template <typename T>
+    typename std::enable_if<std::is_null_pointer<T>::value == true, void>::type
+    print_internal(T value) {
+      std::cout << std::string(indent_, ' ') << "null" << (newline_ ? "\n" : "");
+    }
+
     void print_internal(float value) {
       std::cout << std::string(indent_, ' ') << value << 'f' << (newline_ ? "\n" : "");
-    }    
+    }
 
     void print_internal(double value) {
       std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
     }
 
+    void print_internal(const std::string& value) {
+      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    }
+    
+    void print_internal(const char * value) {
+      std::cout << std::string(indent_, ' ') << value << (newline_ ? "\n" : "");
+    }    
+
     void print_internal(bool value) {
       std::cout << std::string(indent_, ' ') <<
 	(value ? "true" : "false") << (newline_ ? "\n" : "");
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_class<T>::value == true, void>::type
+    print_internal(T value) {
+      std::cout << std::string(indent_, ' ') << type(value) << (newline_ ? "\n" : "");
+    }
+
+    std::string demangle(const char* name) {
+#ifdef __GNUG__
+      int status = -4;      
+      std::unique_ptr<char, void(*)(void*)> res {
+        abi::__cxa_demangle(name, NULL, NULL, &status),
+	  std::free
+	  };
+      return (status==0) ? res.get() : name;
+#else
+      return name;
+#endif      
+    }
+
+    template <class T>
+    std::string type(const T& t) {
+      return demangle(typeid(t).name());
     }
     
     size_t indent_;
