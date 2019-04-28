53e4311ab1662fa54ac3c54a246824481be3c828
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-04-28 08:56:12 -0400

    More unit tests for std::vector

--- test/test_vector.hpp
+++ test/test_vector.hpp
@@ -59,3 +59,78 @@ TEST_CASE("Print simple vector of three elements (line_terminator = '\n', indent
 
   REQUIRE(stream.str() == expected);
 }
+
+TEST_CASE("Print vector of vectors (line_terminator = '\n', indent = 2)",
+	  "[std::vector]") {
+
+  std::stringstream stream;
+  pprint::PrettyPrinter printer(stream);
+  printer.print(std::vector<std::vector<int>>{{1, 2, 3}, {4}, {5, 6}, {}});
+
+  const std::string expected = "[\n"
+    "  [1, 2, 3], \n" 
+    "  [4], \n" 
+    "  [5, 6], \n"
+    "  []"
+    "\n]\n";
+
+  REQUIRE(stream.str() == expected);
+}
+
+TEST_CASE("Print nested vector (line_terminator = '\n', indent = 2)",
+	  "[std::vector]") {
+
+  std::stringstream stream;
+  pprint::PrettyPrinter printer(stream);
+  printer.print(std::vector<std::vector<int>>{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+  const std::string expected = "[\n"
+    "  [1, 2, 3], \n" 
+    "  [4, 5, 6], \n" 
+    "  [7, 8, 9]"
+    "\n]\n";
+
+  REQUIRE(stream.str() == expected);
+}
+
+TEST_CASE("Compact Print nested vector (line_terminator = '\n', indent = 2)",
+	  "[std::vector]") {
+
+  std::stringstream stream;
+  pprint::PrettyPrinter printer(stream);
+  printer.compact(true);
+  printer.print(std::vector<std::vector<int>>{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+  const std::string expected = "[[1, 2, 3], [4, 5, 6], [7, 8, 9]]\n"; 
+
+  REQUIRE(stream.str() == expected);
+}
+
+TEST_CASE("Compact Print empty 3D matrix (line_terminator = '\n', indent = 2)",
+	  "[std::vector]") {
+
+  std::stringstream stream;
+  pprint::PrettyPrinter printer(stream);
+  printer.compact(true);
+  printer.print(std::vector<std::vector<std::vector<int>>>{});
+
+  const std::string expected = "[]\n"; 
+
+  REQUIRE(stream.str() == expected);
+}
+
+TEST_CASE("Print another nested matrix (line_terminator = '\n', indent = 2)",
+	  "[std::vector]") {
+
+  std::stringstream stream;
+  pprint::PrettyPrinter printer(stream);
+  printer.print(std::vector<std::vector<std::vector<int>>>{ {{1, 2, 3}, {4, 5, 6}},
+	{{7, 8, 9}, {10, 11, 12}} });
+
+  const std::string expected = "[\n"
+    "  [[1, 2, 3], [4, 5, 6]], \n"
+    "  [[7, 8, 9], [10, 11, 12]]"
+    "\n]\n";
+
+  REQUIRE(stream.str() == expected);
+}
