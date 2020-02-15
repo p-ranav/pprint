93fca646a9bca349bbe69696a8b8f48436aaca39
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-15 15:58:55 +0100

    conan: conan 'create .' will now build the recipe

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,15 @@ class PprintConan(ConanFile):
     homepage = "https://github.com/p-ranav/pprint"
     url = "https://github.com/p-ranav/pprint"
     license = "MIT"
-    exports_sources = "include/pprint.hpp"
+    exports_sources = "include/**", "test/**", "CMakeLists.txt", "LICENSE"
+    exports = "LICENSE"
     no_copy_source = True
+
+    def set_version(self):
+        import re
+        m = re.search(r"project\(.*VERSION ([0-9.]+)", open(os.path.join(self.recipe_folder, "CMakeLists.txt")).read())
+        assert m, "Could not extract version from CMakeLists.txt"
+        self.version = m.group(1)
 
     _cmake = None
 
@@ -19,6 +26,7 @@ class PprintConan(ConanFile):
             return self._cmake
         self._cmake = CMake(self)
         self._cmake.configure()
+        return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
